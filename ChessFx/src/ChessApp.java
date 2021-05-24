1bcd2c9a5c82d8729fb5c8589a21bc193938f53f
ryan <rdstew11@gmail.com>
2021-05-24 18:18:17 -0400

    init

---
+++ ChessFx/src/ChessApp.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChessApp extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
